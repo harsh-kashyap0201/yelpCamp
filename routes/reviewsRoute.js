94ae6255d7e42db410efb437b90c385c06f3ad29
harsh <harshkashyap2012002@gmail.com>
2022-07-05 23:43:09 +0530

    test6

--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -1,13 +1,8 @@
 const express=require("express");
 const router=express.Router({mergeParams: true});
+const CatchAsync=require("../utils/catchAsync");
 const {isLoggedin,isReviewAuthor} = require("../middleware");
 const reviewController=require("../controllers/review");
-
-CatchAsync=()=>{
-    return(req,res,next)=>{
-        func(req,res,next).catch(next);
-    }
-};
 
 //adding reviews
 router.post("/",isLoggedin,CatchAsync(reviewController.addReview));    
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,8 @@
 const express=require("express");
 const router=express.Router({mergeParams: true});
+const CatchAsync=require("../utils/catchAsync");
 const passport=require("passport");
 const userController=require("../controllers/user");
-
-CatchAsync=()=>{
-    return(req,res,next)=>{
-        func(req,res,next).catch(next);
-    }
-};
 
 //signup route
 router.route("/signup")

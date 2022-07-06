b64369fc177de20688e0f1e3ec2e38a8f2df65d7
harsh <harshkashyap2012002@gmail.com>
2022-07-06 14:09:58 +0530

    utils files renamed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const ejsMate=require("ejs-mate");
 const methodOverride=require('method-override');
 //async wrapper function to handle async errors
 //handling errors
-const ExpressError=require('./utils/ExpressError');
+const ExpressError=require('./utils/expressError');
 //session management
 const session=require('express-session');
 //flash messages
